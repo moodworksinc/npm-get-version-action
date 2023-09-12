b9c50bb3bcbddce0253ac5260d78da159aaa1876
russriser <66765028+russriser@users.noreply.github.com>
2023-09-11 22:03:29 -0500

    Update var names

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ echo "Reading package.json from ${PACKAGE_JSON_PATH}/package.json"
 PACKAGE_VERSION=$(cat ${PACKAGE_JSON_PATH}/package.json | jq '.version' | tr -d '"')
 PACKAGE_RUNTIME=$(cat ${PACKAGE_JSON_PATH}/package.json | jq '.runtime' | tr -d '"')
 
-echo "versionMajor=$(cut -d '.' -f 1 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
-echo "versionMinor=$(cut -d '.' -f 1,2 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
-echo "versionFull=${PACKAGE_VERSION}" >> $GITHUB_OUTPUT
+echo "majorVersion=$(cut -d '.' -f 1 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
+echo "minorVersion=$(cut -d '.' -f 1,2 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
+echo "fullVersion=${PACKAGE_VERSION}" >> $GITHUB_OUTPUT
 
-echo "runtimeMajor=$(cut -d '.' -f 1 <<< ${PACKAGE_RUNTIME})" >> $GITHUB_OUTPUT
-echo "runtimeMinor=$(cut -d '.' -f 1,2 <<< ${PACKAGE_RUNTIME})" >> $GITHUB_OUTPUT
-echo "runtimeFull=${PACKAGE_RUNTIME}" >> $GITHUB_OUTPUT
+echo "majorRuntime=$(cut -d '.' -f 1 <<< ${PACKAGE_RUNTIME})" >> $GITHUB_OUTPUT
+echo "minorRuntime=$(cut -d '.' -f 1,2 <<< ${PACKAGE_RUNTIME})" >> $GITHUB_OUTPUT
+echo "fullRuntime=${PACKAGE_RUNTIME}" >> $GITHUB_OUTPUT
 
