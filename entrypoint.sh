043a99087e3b350aaa73e93b27cc299570d724e1
russriser <66765028+russriser@users.noreply.github.com>
2023-09-11 21:48:06 -0500

    Update entrypoint.sh with runtime

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,13 @@
 PACKAGE_JSON_PATH="${1-.}"
 echo "Reading package.json from ${PACKAGE_JSON_PATH}/package.json"
 PACKAGE_VERSION=$(cat ${PACKAGE_JSON_PATH}/package.json | jq '.version' | tr -d '"')
+PACKAGE_RUNTIME=$(cat ${PACKAGE_JSON_PATH}/package.json | jq '.runtime' | tr -d '"')
 
-echo "major=$(cut -d '.' -f 1 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
-echo "minor=$(cut -d '.' -f 1,2 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
-echo "full=${PACKAGE_VERSION}" >> $GITHUB_OUTPUT
+echo "versionMajor=$(cut -d '.' -f 1 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
+echo "versionMinor=$(cut -d '.' -f 1,2 <<< ${PACKAGE_VERSION})" >> $GITHUB_OUTPUT
+echo "versionFull=${PACKAGE_VERSION}" >> $GITHUB_OUTPUT
+
+echo "runtimeMajor=$(cut -d '.' -f 1 <<< ${PACKAGE_RUNTIME})" >> $GITHUB_OUTPUT
+echo "runtimeMinor=$(cut -d '.' -f 1,2 <<< ${PACKAGE_RUNTIME})" >> $GITHUB_OUTPUT
+echo "runtimeFull=${PACKAGE_RUNTIME}" >> $GITHUB_OUTPUT
+
